4c88e5697a23f170bb5601378851c579e4d9b868
nilavya2000 <dnilavya@gmail.com>
2020-11-23 21:43:00 +0530

    added_create_

--- linklist/insert.cpp
+++ linklist/insert.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-#include <stdio.h>
 using namespace std;
 
-struct node{
-	int info;
+struct node
+{
+	int data;
 	struct node *next;
 };
-
 struct node *head=NULL;
 
 void create()
@@ -15,28 +14,70 @@ void create()
 	temp = (struct node *)malloc(sizeof(struct node));
 	if(temp==NULL)
 	{
-		printf("\n out of memeory..!! \n");
+		cout<<"Out of memory.. !! \n";
 		exit(0);
 	}
-	printf("Enter the value of the node: ");
-	scanf("%d",&temp->info);
+	cout<<"Enter the data value for the node : ";
+	cin>>temp->data;
 	temp->next=NULL;
 	if(head==NULL)
+	{
 		head=temp;
+	}
 	else
 	{
-		ptr=head;
-		while(ptr->next!=NULL)
+		ptr = head;
+		while(ptr->next !=NULL)
 			ptr=ptr->next;
-		ptr->next = temp;
+		ptr->next=temp;
 	}
-
 }
 
-
+void display()
+{
+	struct node *ptr;
+	if(head == NULL)
+	{
+		cout<<"Empty !!";
+	}
+	else
+	{
+		ptr = head;
+		cout<<"\n Elements are : ";
+		while(ptr!=NULL)
+		{
+			cout<<"\t"<<ptr->data;
+			ptr=ptr->next;
+		}
+	}
+}
 
 
 int main()
 {
-
+	int ch;
+	while(1)
+	{
+		cout<<"\n MENU \n";
+		cout<<"1.Create\n";
+		cout<<"2.Display\n";
+		cout<<"3.exit";
+		cin>>ch;
+		switch(ch)
+		{
+			case 1:
+				create();
+				break;
+			case 2:
+				display();
+				break;
+			case 3:
+				exit(0);
+				break;
+			default:
+				cout<<"wrong choice .!!";
+				break;
+		}
+	}
+	return 0;
 }
